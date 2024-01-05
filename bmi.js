6ae401b478ce37a2de92bafafc8746834ed182af
kette-kt <149694439+kette-kt@users.noreply.github.com>
2024-01-05 22:59:52 +0900

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,15 @@
+// 定数の宣言(体重)
+let userWeight;
+// 値の代入
+userWeight = 68;
+
+// 変数の宣言・値の代入(身長)
+let userHight = 1.7;
+
+// 変数の宣言・値の代入(BMI)
+let bmi = userWeight / (userHight * userHight);
+
+//コンソールへの出力
+console.log(bmi);
+
+
